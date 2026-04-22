ea8010d1cde77aa89a2d0e15b2584177abc08761
augustkeller <guskeller@gmail.com>
2026-04-22 11:59:01 -0500

    exercise 1 prepped

---
+++ java-codewars.js
@@ -0,0 +1,12 @@
+//Exercise 1
+//Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+//Example:
+// Inputs: "abc", "bc"
+// Output: true
+// Inputs: "abc", "d"
+// Output: false
+public class Kata {
+    public static boolean solution(String str, String ending) {
+        return false;
+    }
+}
